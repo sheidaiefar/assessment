a6d1e11ac59d9ed40e7b45519d0acc659e0ca063
malihe <sheidaiefar@gmail.com>
2023-03-24 18:34:54 +0430

    repeater

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,22 @@ export class AppComponent {
     { name: 'B', job: 'E' },
     { name: 'C', job: 'E' },
   ];
+
+  sortingData = [
+    {
+      name: 'malihe',
+      age: 22,
+      birthDate: new Date(29, 1, 2000),
+    },
+    {
+      name: 'anna',
+      age: 72,
+      birthDate: new Date(29, 1, 1930),
+    },
+    {
+      name: 'tida',
+      age: 2,
+      birthDate: new Date(29, 1, 2020),
+    },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RepeatComponent } from './repeat/repeat.component';
-import { RepeatDirective } from './repeat/repeat.directive';
+import { RepeatComponent } from './components/repeat/repeat.component';
+import { RepeatDirective } from './components/repeat/repeat.directive';
 
 @NgModule({
   declarations: [
