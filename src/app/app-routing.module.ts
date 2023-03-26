dcf5dff3df0021c765646de12917b422b88b700e
malihe <sheidaiefar@gmail.com>
2023-03-26 19:25:51 +0430

    pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AssessmentComponent } from './pages/assessment/assessment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/assessment'
+  },
+  {
+    path: 'assessment',
+    component: AssessmentComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/assessment/assessment.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AssessmentComponent } from './assessment.component';
+
+
+@NgModule({
+  declarations: [
+    AssessmentComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class AssessmentModule { }
